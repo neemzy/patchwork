53f1ca8b1346518b3618d1403d697bc02041cd75
neemzy <tom.panier@free.fr>
2012-10-28 07:45:45 +0100

    fixed model qualified names

--- src/Patchwork/Helper/Bean.php
+++ src/Patchwork/Helper/Bean.php
@@ -6,7 +6,7 @@ class Bean extends \RedBean_BeanHelper_Facade
 {
 	public function getModelForBean(\RedBean_OODBBean $bean)
 	{
-		$modelName = 'Patchwork\\'.\RedBean_ModelHelper::getModelName($bean->getMeta('type'), $bean);
+		$modelName = 'Patchwork\\Model\\'.ucfirst($bean->getMeta('type'));
 		if ( ! class_exists($modelName))
 			return null;
 		$obj = \RedBean_ModelHelper::factory($modelName);
---
+++ src/Patchwork/Model/Pizza.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Patchwork\Model;
+
+class Pizza extends \RedBean_SimpleModel
+{
+
+}
