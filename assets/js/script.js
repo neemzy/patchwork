c5f95e27a657d2165da87db5d7312e1f92d69ca0
Tom Panier <tom.panier@free.fr>
2012-11-08 15:10:20 +0100

    final js clean

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,8 @@
     // HTML5 placeholder polyfill
     function initPlaceHolders()
     {
-        if (!('placeholder' in document.createElement('input'))) {
+        if (!('placeholder' in document.createElement('input')))
+        {
             $('input[type=password][placeholder]').attr('placeholder', $('input[type=password][placeholder]').eq(0).attr('placeholder'));
             var items = $('[placeholder]');
             items.each(function()
@@ -36,53 +37,6 @@
     
     // jQuery outerHTML
     $.fn.outer = function() { return $($('<div></div>').html(this.clone())).html(); }
-
-    // Slider (part 1)
-    var busy = false, sObj, sDots, toMove, slideWidth, slideNb, slideDelay = 5000, isAuto, autoSlide, currentSlide = 1;
-    function slideIt(slideToGo)
-    {
-        if (!busy)
-        {
-            busy = true;
-            if (slideToGo == null) slideToGo = true;
-            if (isNaN(parseInt(slideToGo)))
-            {
-                var nextMove = '-='+slideWidth;
-                if (slideToGo)
-                    currentSlide++;
-                else
-                {
-                    nextMove = '+='+slideWidth;
-                    currentSlide--;
-                    if (currentSlide < 1)
-                    {
-                        currentSlide = slideNb;
-                        nextMove = ((-1) * (currentSlide - 1) * slideWidth);
-                    }
-                }
-            }
-            else
-            {
-                nextMove = ((-1) * slideToGo * slideWidth);
-                currentSlide = (slideToGo + 1);
-            }
-            if ((currentSlide > slideNb) || (currentSlide < 1))
-            {
-                nextMove = '0';
-                currentSlide = 1;
-            }
-            toMove.animate({ 'left': nextMove+'px' }, 'linear', function()
-            {
-                busy = false;
-                sDots.removeClass('current').eq(currentSlide - 1).addClass('current');
-                if (isAuto)
-                {
-                    clearInterval(autoSlide);
-                    autoSlide = setInterval('slideIt(true)', slideDelay);
-                }
-            });
-        }
-    }
     
 
 
@@ -101,21 +55,6 @@
             that.attr('rel', function(i, val) { return ((typeof val === 'undefined') ? '' : val+' ')+that.data('rel'); }).removeAttr('data-rel');
         });
 
-        // Slider (part 2)
-        sObj = $('#slider');
-        sDots = sObj.find('.dot');
-        isAuto = sObj.hasClass('auto');
-        if (sObj.length == 1)
-        {
-            toMove = sObj.find('ul').eq(0);
-            slideWidth = sObj.width();
-            slideNb = toMove.children().length;
-            sObj.find('.arrow').on('click', function() { slideIt($(this).is('.arrow + .arrow')); });
-            sDots.on('click', function() { slideIt(sDots.index($(this))); }).eq(0).addClass('current');
-            if (isAuto)
-                autoSlide = setInterval('slideIt(true)', slideDelay);
-        }
-
         // lte IE 8
         if ($('body').is('ie6, ie7'))
         {
@@ -129,13 +68,6 @@
     // Loading complete
     $(window).load(function()
     {
-        var obf, obw, ftr = $('body > footer');
-        if (ftr.length > 0)
-        {
-            obf = (ftr.offset().top + ftr.outerHeight());
-            obw = $(window).height();
-            if (obf < obw)
-                ftr.height(ftr.height() + (obw - obf));
-        }
+        
     });
 })(jQuery);
