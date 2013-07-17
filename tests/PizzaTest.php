f0fcc93fd579889f862db5289562608242eac72d
Tom Panier <tom.panier@free.fr>
2013-07-17 14:23:21 +0200

    added sqlite database for tests

--- tests/PizzaTest.php
+++ tests/PizzaTest.php
@@ -1,15 +1,19 @@
 <?php
 
 require_once(dirname(__DIR__).'/vendor/autoload.php');
-require_once(dirname(__DIR__).'/config.php');
+
+use Patchwork\Helper\RedBean as R;
 
 class PizzaTest extends PHPUnit_Framework_TestCase
 {
     protected function setUp()
     {
-        $class = MODEL_NAMESPACE.'Pizza';
-        $this->pizza = new $class;
+        R::setup('sqlite:test.db');
+        R::$toolbox->getRedBean()->setBeanHelper(new Patchwork\Helper\BeanHelper());
+        $this->pizza = R::dispense('pizza');
     }
+
+
 
     public function testCanCreateAPizza()
     {
