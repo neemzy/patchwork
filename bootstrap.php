224ed6d55c4bc4e9f9418b4ea9258dac9e456038
neemzy <tom.panier@free.fr>
2013-09-01 10:59:41 +0200

    solved base path constant location bug

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,5 @@
 <?php
 
-define('BASE_PATH', __DIR__);
 require_once(BASE_PATH.'/vendor/autoload.php');
 
 use Symfony\Component\HttpFoundation\Session\Session;
--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
 
+define('BASE_PATH', dirname(__DIR__));
+
 require_once(BASE_PATH.'/bootstrap.php');
 
 $app->run();
