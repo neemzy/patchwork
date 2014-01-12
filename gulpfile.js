16983d76c4590dc797a6c0a0d9c0223bdd08054a
neemzy <tom.panier@free.fr>
2014-01-12 23:03:01 +0100

    switched to gulp (still minor issues awaiting a fix)

---
+++ gulpfile.js
@@ -0,0 +1,107 @@
+var gulp = require('gulp'),
+    tasks = require('gulp-load-tasks')(),
+    server = require('tiny-lr')();
+
+
+
+gulp.task('rimraf', function () {
+    gulp.src('public/assets/', { read: false })
+        .pipe(tasks.rimraf());
+});
+
+
+
+gulp.task('phpunit', function () {
+    gulp.src('app/tests/')
+        .pipe(tasks.exec('phpunit <%= file.path %>'));
+});
+
+
+
+gulp.task('css', function () {
+    gulp.src('app/assets/less/*.less') // https://github.com/plus3network/gulp-less/pull/21
+        .pipe(tasks.less())
+        .pipe(tasks.autoprefixer())
+        .pipe(tasks['if'](gulp.env.production, tasks.csso()))
+        .pipe(gulp.dest('public/assets/css/'))
+        .pipe(tasks.livereload(server));
+});
+
+
+
+gulp.task('js', function () {
+    gulp.src('app/assets/js/**/*.js')
+        .pipe(tasks.jshint())
+        .pipe(tasks.jshint.reporter('default'))
+        .pipe(tasks.browserify())
+        .pipe(tasks['if'](gulp.env.production, tasks.uglify()))
+        .pipe(gulp.dest('public/assets/js/'))
+        .pipe(tasks.livereload(server));
+
+    gulp.src('vendor/neemzy/patchwork-core/assets/js/nicEdit.js')
+        .pipe(gulp.dest('public/assets/js/'));
+});
+
+
+
+gulp.task('img', function () {
+    gulp.src(['app/assets/img/**/*', 'vendor/neemzy/patchwork-core/assets/img/**/*'])
+        .pipe(tasks['if'](gulp.env.production, tasks.imagemin()))
+        .pipe(gulp.dest('public/assets/img/'))
+        .pipe(tasks.livereload(server));
+});
+
+
+
+gulp.task('font', function () {
+    var from = 'app/assets/font/*.ttf',
+        to = 'public/assets/font/';
+
+    gulp.src(from, { buffer: false })
+        .pipe(gulp.dest(to));
+
+    gulp.src(from, { buffer: false })
+        .pipe(tasks.ttf2woff())
+        .pipe(gulp.dest(to));
+
+    gulp.src(from, { buffer: false })
+        .pipe(tasks.ttf2eot())
+        .pipe(gulp.dest(to));
+});
+
+
+
+gulp.task('icon', function () {
+    gulp.src(['bower_components/bootstrap/dist/fonts/*', '!bower_components/bootstrap/dist/fonts/*.svg'], { buffer: false })
+        .pipe(gulp.dest('public/assets/font/'))
+});
+
+
+
+gulp.task('workflow', function () {
+    gulp.src('gulpfile.js')
+        .pipe(open('', { url: 'http://www.patch.work/' }));
+
+    server.listen(35727, function (err) {
+        gulp.watch('app/assets/less/**/*.less', function () {
+            gulp.run('css');
+        });
+
+        gulp.watch('app/assets/js/**/*.js', function () {
+            gulp.run('js');
+        });
+
+        gulp.watch('app/assets/img/**/*', function () {
+            gulp.run('img');
+        });
+    });
+});
+
+
+
+gulp.task('default', function() {
+    gulp.run('phpunit');
+    //gulp.run('rimraf');
+    gulp.run('css', 'js', 'img', 'font', 'icon');
+    gulp.env.production || gulp.run('workflow');
+});
